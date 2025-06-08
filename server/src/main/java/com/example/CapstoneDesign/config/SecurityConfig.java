3856f08499d7f6ab941b5f32004ca240ffdf48e5
qorcodusl <qorcodus03@naver.com>
2025-06-09 00:39:12 +0900

    Fix: CORS 설정

--- server/src/main/java/com/example/CapstoneDesign/config/SecurityConfig.java
+++ server/src/main/java/com/example/CapstoneDesign/config/SecurityConfig.java
@@ -22,7 +22,8 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
-                .cors(cors -> cors.disable())
+                .cors()
+                .and()
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/**",
